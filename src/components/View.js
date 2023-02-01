a21914d21133558c644b0b9169c911801ed2558f
Rokoos <marcin.widomski83@gmail.com>
2023-02-01 22:20:09 +0100

    Setting View photos

--- src/components/View.js
+++ src/components/View.js
@@ -1,7 +1,8 @@
 import { useRef } from "react";
 import { MDBView, MDBMask } from "mdbreact";
 import Theme1 from "../images/aviation.jpg";
-// Photo by Andrés Dallimonti on Unsplash
+// Photo by <a href="https://unsplash.com/@richardrschunemann?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Richard R. Schünemann</a> on <a href="https://unsplash.com/photos/0-CFHP2it0w?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
 import Theme2 from "../images/aviation2.jpg";
 // Zdjęcie dodane przez Pixabay: https://www.pexels.com/pl-pl/zdjecie/statki-powietrzne-w-ciagu-dnia-40907/
 
@@ -13,10 +14,10 @@ const View = () => {
   return (
     <div className="container p-0">
       <MDBView src={photo}>
-        <MDBMask overlay="black-strong" className="mask_text text-white">
-          <h2 className="view_font pb-4">
+        <MDBMask className="mask_text text-white">
+          <span className="view_font pb-4">
             Let's write about aviation books...
-          </h2>
+          </span>
         </MDBMask>
       </MDBView>
     </div>
